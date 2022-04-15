ffc0ac6fc9191310f0da9daad6e3c39620dae00f
Aaron Cain <acain@expediagroup.com>
2022-04-14 17:27:12 -0700

    Adding Readme link
    Listing out rules and run rules

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Build and run the App inspect rules
 
 ``` build_run.sh ```
 
+https://dev.splunk.com/enterprise/reference/appinspect/appinspectcliref
--- Scripts/validate.sh
+++ Scripts/validate.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+echo "================================================================================"
+echo "appinspect version"
+echo "================================================================================"
 splunk-appinspect list version
+
+echo ""
+splunk-appinspect list checks --custom-checks-dir Rules --included-tags rule1 --excluded-tags splunk-appinspect
+
+
+echo ""
+echo "================================================================================"
+echo "Run rules1"
+echo "================================================================================"
+splunk-appinspect inspect AppList/app-1   --custom-checks-dir Rules --included-tags rule1 --excluded-tags splunk-appinspect  --data-format json --output-file results.json
 
